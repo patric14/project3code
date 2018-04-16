f44b536ac5656aa5beeac5854e940be27a37b7d1
Caleb Patrick <patric14@purdue.edu>
2018-04-16 16:55:18 -0400

    pid controller
    added pid controller test file

---
+++ pid_controller.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import robot_team17
+import time
+import brickpi3
+
+robot = robot_team17.RobotLibrary()
+BP = brickpi3.BrickPi3
+
+TARGET = 5
+dT = .2
+KP = robot.getval('Input kP: ')
+
+power = 50
+
+try:
+    while True:
+        currentDist = robot.check_distance()
+
+        e = TARGET - currentDist
+
+        P = -1 * KP * e
+
+        BP.set_motor_power(BP.PORT_D, power - P)
+        BP.set_motor_power(BP.PORT_A, power + P)
+        
+        time.sleep(dT)
+except KeyboardInterrupt:
+    pass
+
+robot.kill()
