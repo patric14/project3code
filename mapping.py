a1763286cef0c20300d3a4effd5dab02abc7edc0
Zachary J McClary <zmcclary@purdue.edu>
2018-04-17 18:33:57 -0400

    Constant

--- mapping.py
+++ mapping.py
@@ -1,10 +1,10 @@
 import robot_team17
 import time
+IN2FT = 12
+mapLengthX = 22 * IN2FT #in
+mapLengthY = 12 * IN2FT #in
 
-mapLengthX = 22 * 12 #in
-mapLengthY = 12 * 12 #in
-
-map_number, unit_length, unit, origin, notes = setup(self)
+map_number, unit_length, unit, origin, notes = setup()
 
 mapBlockX = mapLengthX / unit_length
 mapBlockY = mapLengthY / unit_length
