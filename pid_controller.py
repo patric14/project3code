525312f38e890944ac253f3c1f491ae067b338cc
Zachary J McClary <zmcclary@purdue.edu>
2018-04-17 17:10:21 -0400

    Fix Wall Boi SINtax

--- pid_controller.py
+++ pid_controller.py
@@ -14,7 +14,7 @@ power = 50
 
 previousDist = robot.check_distance()
 positionPreviousLeft = BP.get_motor_encoder(BP.Port_D)
-positionPreviousRight = BP..get_motor_encoder(BP.Port_A)
+positionPreviousRight = BP.get_motor_encoder(BP.Port_A)
 
 try:
     while True:
@@ -22,7 +22,7 @@ try:
         positionCurrentLeft = BP.get_motor_encoder(BP.Port_D)
         positionCurrentRight = BP.get_motor_encoder(BP.Port_A)
 
-        diffDist = currentDist - previousDist
+        distDiff = currentDist - previousDist
         
         leftDistDrive = robot.DIST_DEG * (positionCurrentLeft - positionPreviousLeft)
         rightDistDrive = robot.DIST_DEG * (positionCurrentRight - positionPreviousRight)
@@ -37,9 +37,9 @@ try:
         degrees = (1 + K) * abs(angle)
         
         if (angle > 0) and (P > 0):
-            robot.turn(self, 0, degrees, 100)
+            robot.turn(0, degrees, 100)
         if (angle < 0) and (P < 0):
-            robot.turn(self, 1, degrees, 100)
+            robot.turn(1, degrees, 100)
 
         BP.set_motor_power(BP.PORT_D, power + P, power + P)
         BP.set_motor_power(BP.PORT_A, power - P, power - P)
