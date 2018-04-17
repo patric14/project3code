baa31e6ab4ef280b20a3afa2805ae5ee4cccc08e
Zachary J McClary <zmcclary@purdue.edu>
2018-04-16 20:37:12 -0400

    PID Update and Mapping Addition

---
+++ mapping.py
@@ -0,0 +1,14 @@
+import robot_team17
+import time
+
+mapLengthX = 22 * 12 #in
+mapLengthY = 12 * 12 #in
+
+map_number, unit_length, unit, origin, notes = setup(self)
+
+mapBlockX = mapLengthX / unit_length
+mapBlockY = mapLengthY / unit_length
+
+map = mapSetup(mapBlockX, mapBlockY)
+
+
--- pid_controller.py
+++ pid_controller.py
@@ -1,6 +1,7 @@
 import robot_team17
 import time
 import brickpi3
+from math import atan, cos, abs
 
 robot = robot_team17.RobotLibrary()
 BP = brickpi3.BrickPi3
@@ -11,18 +12,42 @@ KP = robot.getval('Input kP: ')
 
 power = 50
 
+previousDist = robot.check_distance()
+positionPreviousLeft = BP.get_motor_encoder(BP.Port_D)
+positionPreviousRight = BP..get_motor_encoder(BP.Port_A)
+
 try:
     while True:
         currentDist = robot.check_distance()
+        positionCurrentLeft = BP.get_motor_encoder(BP.Port_D)
+        positionCurrentRight = BP.get_motor_encoder(BP.Port_A)
 
-        e = TARGET - currentDist
+        diffDist = currentDist - previousDist
+        
+        leftDistDrive = robot.DIST_DEG * (positionCurrentLeft - positionPreviousLeft)
+        rightDistDrive = robot.DIST_DEG * (positionCurrentRight - positionPreviousRight)
+        distDrive = (leftDistDrive + rightDistDrive) / 2
+        
+        angle = atan(distDiff / distDrive)
+        
+        distPerp = currentDist * cos(abs(angle))
+        
+        P = (distPerp - 5)
+        K = P / 5
+        degrees = (1 + K) * abs(angle)
+        
+        if (angle > 0) and (P > 0):
+            robot.turn(self, 0, degrees, 100)
+        if (angle < 0) and (P < 0):
+            robot.turn(self, 1, degrees, 100)
 
-        P = -1 * KP * e
-
-        BP.set_motor_power(BP.PORT_D, power - P, power - P)
-        BP.set_motor_power(BP.PORT_A, power + P, power + P)
+        BP.set_motor_power(BP.PORT_D, power + P, power + P)
+        BP.set_motor_power(BP.PORT_A, power - P, power - P)
 
         time.sleep(dT)
+
+        previous = currentDist
+
 except KeyboardInterrupt:
     pass
 
