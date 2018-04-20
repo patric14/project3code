3663a923bfaed6323bce717fa0b1028f13dd0b8f
Caleb Patrick <patric14@purdue.edu>
2018-04-20 10:10:12 -0400

    test

---
+++ team17_mapping.py
@@ -0,0 +1,39 @@
+# Project 3
+# File: team17_mapping.py
+# Date: 2018
+# By: Abby Beard
+# beard29
+# Caleb Patrick
+# patric14
+# Haydn Schroader
+# hschroad
+# Zach McClary
+# zmcclary
+# Section: 1
+#
+# Team: Team Number #
+# ELECTRONIC SIGNATURE
+# Abby Beard
+# Caleb Patrick
+# Haydn Schroader
+# Zach McClary
+#
+# The electronic signatures above indicate that the program
+# submitted for evaluation is the combined effort of all
+# team members and that each member of the team was an
+# equal participant in its creation. In addition, each
+# member of the team has a general understanding of
+# all aspects of the program development and execution.
+#
+# This script runs the mapping routine for the Team 17 robot.
+
+import robot_team17
+robot = robot_team17.RobotLibrary()
+
+fully_mapped = False
+explore_space = False
+
+robot.setup()
+
+'''while explore_space == False:
+'''
