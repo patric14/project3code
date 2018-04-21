baa75254af8fd705961848255ec918c0b7af824c
Caleb Patrick <patric14@purdue.edu>
2018-04-21 16:00:11 -0400

    rando test changes

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import robot_team17
 import time
+import brickpi3
 robot = robot_team17.RobotLibrary()
+BP = brickpi3.BrickPi3()
 
 try:
-    robot.drive_dist(1, 15, 40)
+    BP.offset_motor_encoder(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C))
+    while True:
+        print(BP.get_motor_encoder(BP.PORT_C))
+        time.sleep(.5)
 except KeyboardInterrupt:
     pass
 
