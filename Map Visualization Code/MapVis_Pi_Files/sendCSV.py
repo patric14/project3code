a53dbb4466d3f16a2ecf331c091acd97bf509a3d
Caleb Patrick <patric14@purdue.edu>
2018-04-23 11:13:41 -0400

    added ip and file path to map vis

--- Map Visualization Code/MapVis_Pi_Files/sendCSV.py
+++ Map Visualization Code/MapVis_Pi_Files/sendCSV.py
@@ -1,13 +1,13 @@
 # Sends the map to the computer using the CSV file on the Pi
 # Run with recieveCSV.py on the computer.
-# 
+#
 # Update the IPv4 address in line 10 as needed.
 
 from piTalk import connect, sendMapFile
 import time
 import csv
 
-ipv4_address = '192.168.XX.YY'
+ipv4_address = '192.168.17.21'
 refreshRate = 1 # second(s)
 showRawSendData = False
 
@@ -19,7 +19,7 @@ while True:
     map = []
     count = 0
 
-    mapFile = open('/home/mk_99/Desktop/map.csv', 'r')
+    mapFile = open('/home/team_17/Desktop/map.csv', 'r')
     reader = csv.reader(mapFile)
     for row in reader:
       if (count < 6):
@@ -36,7 +36,7 @@ while True:
               tempRow.append(c)
           map.append(tempRow)
       count += 1
-  
+
     mapFile.close()
     sendMapFile(map, showData=showRawSendData)
     startTime = time.time()
