ce28cfa0f20517f0059a3b2942f2cb13c00d4c93
Caleb Patrick <patric14@purdue.edu>
2018-04-24 19:06:49 -0400

    recommit

--- test.py
+++ test.py
@@ -5,13 +5,8 @@ robot = meme.RobotLibrary()
 BP = brickpi3.BrickPi3()
 
 try:
-<<<<<<< HEAD
     robot.check_junction(30)
 
-=======
-    while True:
-        robot.drive_dist(1, 40)
->>>>>>> 9ad743a2abee20f476b3903fc31d2c517849e986
 except KeyboardInterrupt:
     pass
 
